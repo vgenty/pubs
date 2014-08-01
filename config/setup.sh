1af2a3a1ffb37e3c649cbb85d7623e0ef4c580f6
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-08-01 07:38:04 -0500

    Add more shell-env control variables --kazu

--- config/setup.sh
+++ config/setup.sh
@@ -1,3 +1,6 @@
+#
+# Python configuration
+#
 
 # If PUB_TOP_DIR not set, try to guess
 if [[ -z $PUB_TOP_DIR ]]; then
@@ -6,8 +9,32 @@ if [[ -z $PUB_TOP_DIR ]]; then
     # Find the directory one above.
     export PUB_TOP_DIR="$( cd "$( dirname "$me" )" && pwd )"
 fi
-
+# Set PYTHONPATH
 export PYTHONPATH=$PUB_TOP_DIR:$PYTHONPATH
+
+
+#
+# Project configuration
+# 
+
+# Default logger level
+export PUB_LOGGER_LEVEL=kLOGGER_DEBUG
+
+# Default message drain
+export PUB_LOGGER_DRAIN=kLOGGER_COUT
+export PUB_LOGGER_FILE_LOCATION=$PUB_TOP_DIR/log
+
+# SQL reader account config
+export PUB_PSQL_READER_HOST=localhost
+export PUB_PSQL_READER_USER=$USER
+export PUB_PSQL_READER_DB=procdb
+export PUB_PSQL_READER_PASS=""
+
+# SQL writer account config
+export PUB_PSQL_WRITER_HOST=localhost
+export PUB_PSQL_WRITER_USER=$USER
+export PUB_PSQL_WRITER_DB=procdb
+export PUB_PSQL_WRITER_PASS=""
 
 
 
