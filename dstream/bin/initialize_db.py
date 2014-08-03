62449de7e86f97169e0b757146e9e0a957d601f6
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-08-03 18:05:42 -0500

    Adding utility python scripts for re-setting DB --kazu

---
+++ dstream/bin/initialize_db.py
@@ -0,0 +1,14 @@
+import sys
+from dstream.ds_api import death_star
+from pub_dbi        import pubdb_conn_info
+from pub_util       import pub_logger
+
+logger = pub_logger.get_logger('death_star')
+k=death_star( pubdb_conn_info.writer_info(),
+              logger )
+
+if not k.connect():
+    sys.exit(1)
+
+k.recreate_death_star()
+
---
+++ dstream/bin/recreate_test_mainrun.py
@@ -0,0 +1,26 @@
+import argparse, sys
+from dstream.ds_api import death_star
+from pub_dbi        import pubdb_conn_info
+from pub_util       import pub_logger
+
+myparser = argparse.ArgumentParser(description='Re-initialize DB with filled MainRun table.')
+
+myparser.add_argument('--nruns', dest='nruns', action='store',
+                      default=0, type=int,
+                      help='Number of runs to fill a new MainRun')
+
+myparser.add_argument('--nsubruns',dest='nsubruns',action='store',
+                      default=0, type=int,
+                      help='Number of sub-runs to fill a new MainRun')
+
+args = myparser.parse_args()
+
+logger = pub_logger.get_logger('death_star')
+k=death_star( pubdb_conn_info.writer_info(),
+              logger )
+
+if not k.connect():
+    sys.exit(1)
+
+k.refill_death_star(args.nruns,args.nsubruns)
+
