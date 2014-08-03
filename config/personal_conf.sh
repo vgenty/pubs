1a2d125dbdac5814be144c993f65303dad751360
drinkingkazu <kazuhiro@nevis.columbia.edu>
2014-08-03 14:12:48 -0500

    commented out but show a possibility of using file output for a logger. Example 'mistaken' SMTP configuration (again commented out) for testing smtp exception handling --kazu

--- config/personal_conf.sh
+++ config/personal_conf.sh
@@ -14,3 +14,7 @@ export PUB_PSQL_WRITER_PASS=""
 export PUB_SMTP_ACCT=drinkingkazu.pubs@gmail.com
 export PUB_SMTP_SRVR=smtp.gmail.com:587
 export PUB_SMTP_PASS=pubs.drinkingkazu
+
+#export PUB_SMTP_ACCT=drinkingkazu.pubs@aho.com
+#export PUB_SMTP_SRVR=smtp.gmail.com:tako
+#export PUB_SMTP_PASS=pubs.drinkingkazuak
