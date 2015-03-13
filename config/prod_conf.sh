3637f5deaac6a3bb04ec8a3a2943e1e144362836
drinkingkazu <kazuhiro@nevis.columbia.edu>
2015-03-13 11:33:43 -0500

    Added  which needs to be your kerberos account name... so kterao even if I log into uboonepro account --kazu

--- config/prod_conf.sh
+++ config/prod_conf.sh
@@ -1,7 +1,7 @@
 # SQL reader account config
 export PUB_PSQL_READER_HOST=ifdb01.fnal.gov
 export PUB_PSQL_READER_PORT=5437
-export PUB_PSQL_READER_USER=$USER
+export PUB_PSQL_READER_USER=$PUB_PROD_ACCOUNT
 export PUB_PSQL_READER_ROLE="uboone_admin"
 export PUB_PSQL_READER_DB=microboone_dev
 export PUB_PSQL_READER_PASS=""
@@ -9,7 +9,7 @@ export PUB_PSQL_READER_PASS=""
 # SQL writer account config
 export PUB_PSQL_WRITER_HOST=ifdb01.fnal.gov
 export PUB_PSQL_WRITER_PORT=5437
-export PUB_PSQL_WRITER_USER=$USER
+export PUB_PSQL_WRITER_USER=$PUB_PROD_ACCOUNT
 export PUB_PSQL_WRITER_ROLE="uboone_admin"
 export PUB_PSQL_WRITER_DB=microboone_dev
 export PUB_PSQL_WRITER_PASS=""
