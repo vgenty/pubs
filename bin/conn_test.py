10b00d4e55ea7f6eb89775c209c92099d9012aed
kterao <kterao@fnal.gov>
2015-04-07 15:00:23 -0500

    Adding connection test script & ubdaq specific configuration --kazu

---
+++ bin/conn_test.py
@@ -0,0 +1,2 @@
+from pub_dbi import pubdb_conn, pubdb_conn_info
+pubdb_conn._connect(pubdb_conn_info.reader_info())
---
+++ config/ubdaq_conf.sh
@@ -0,0 +1,34 @@
+# SQL reader account config
+export PUB_PSQL_READER_HOST=""
+export PUB_PSQL_READER_PORT=""
+export PUB_PSQL_READER_USER=$USER
+export PUB_PSQL_READER_ROLE="uboonedaq_reader"
+export PUB_PSQL_READER_DB=testprocdb
+export PUB_PSQL_READER_PASS=""
+
+# SQL writer account config
+export PUB_PSQL_WRITER_HOST=""
+export PUB_PSQL_WRITER_PORT=""
+export PUB_PSQL_WRITER_USER=$USER
+export PUB_PSQL_WRITER_ROLE="uboonedaq_admin"
+export PUB_PSQL_WRITER_DB=testprocdb
+export PUB_PSQL_WRITER_PASS=""
+
+# SQL writer account config
+export PUB_PSQL_ADMIN_HOST=""
+export PUB_PSQL_ADMIN_PORT=""
+export PUB_PSQL_ADMIN_USER=$USER
+export PUB_PSQL_ADMIN_ROLE="uboonedaq_admin"
+export PUB_PSQL_ADMIN_DB=testprocdb
+export PUB_PSQL_ADMIN_PASS=""
+
+# SMTP account for sending an email report
+export PUB_SMTP_ACCT=uboonepro
+export PUB_SMTP_SRVR=smtp.gmail.com:587
+export PUB_SMTP_PASS=herbgreenlee
+
+export PUB_INDIR="/Users/yuntse/Data/uboone/pubs/test_in"
+export PUB_OUTDIR="/Users/yuntse/Data/uboone/pubs/test_out"
+#export PUB_SMTP_ACCT=drinkingkazu.pubs@aho.com
+#export PUB_SMTP_SRVR=smtp.gmail.com:tako
+#export PUB_SMTP_PASS=pubs.drinkingkazuak
