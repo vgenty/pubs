53b30ff22cc73cfbd78792135182eeb00841046d
kterao <kterao@fnal.gov>
2015-04-08 11:23:03 -0500

    Add server handler --kazu

--- config/setup.sh
+++ config/setup.sh
@@ -14,34 +14,6 @@ fi
 export PGOPTIONS="-c client_min_messages=WARNING";
 
 #
-# Server-specific configuration
-#
-case `uname -n` in
-    (*uboonegpvm*)
-	echo Setting up for uboonegpvm...
-	source /grid/fermiapp/products/uboone/setup_uboone.sh
-	setup psycopg2 v2_5_4
-	setup uboonecode v04_03_01 -q e7:prof
-	;;
-    (*ubdaq-prod*)
-	echo Setting up for ubdaq-prod machines...
-        source /uboone/setup_online.sh
-	source /uboone/larsoft/setup
-	source $PUB_TOP_DIR/config/ubdaq_conf.sh
-	setup uboonecode v03_04_00 -q e6:prof
-        source /home/$USER/development/uboonedaq/projects/cpp2py/config/setup_cpp2py.sh
-	setup git
-	setup sam_web_client
-	setup ifdhc
-	setup psycopg2 v2_5_4
-	setup postgres v9_2_4
-	;;
-    (*)
-	echo No special setup done for the server `uname -n`
-	;;
-esac
-
-#
 # Python configuration
 #
 # Set PATH
@@ -65,5 +37,38 @@ export PUB_LOGGER_FILE_LOCATION=$PUB_TOP_DIR/log
 #
 # Daemon configuration
 #
-export PUB_DAEMON_LOG_MODULE=ds_server_log.ubdaq_smc
+export PUB_DAEMON_LOG_MODULE=""
+export PUB_DAEMON_HANDLER_MODULE=""
+
+#
+# Server-specific configuration
+#
+case `uname -n` in
+    (*uboonegpvm*)
+	echo Setting up for uboonegpvm...
+	source /grid/fermiapp/products/uboone/setup_uboone.sh
+	setup psycopg2 v2_5_4
+	setup uboonecode v04_03_01 -q e7:prof
+	;;
+    (*ubdaq-prod*)
+	echo Setting up for ubdaq-prod machines...
+        source /uboone/setup_online.sh
+	source /uboone/larsoft/setup
+	source $PUB_TOP_DIR/config/ubdaq_conf.sh
+	setup uboonecode v03_04_00 -q e6:prof
+        source /home/$USER/development/uboonedaq/projects/cpp2py/config/setup_cpp2py.sh
+	setup git
+	setup sam_web_client
+	setup ifdhc
+	setup psycopg2 v2_5_4
+	setup postgres v9_2_4
+	export PUB_DAEMON_LOG_MODULE=ds_server_log.ubdaq_logger_smc
+	export PUB_DAEMON_HANDLER_MODULE=ds_server_log.ubdaq_handler_smc
+	;;
+    (*)
+	echo No special setup done for the server `uname -n`
+	;;
+esac
+
+
 
