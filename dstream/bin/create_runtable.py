6006b6e7140f1257153525e5035fd01016e3ec0e
yuntse <yuntse@slac.stanford.edu>
2015-04-15 17:53:20 -0500

    temporary --kaz

---
+++ dstream/bin/create_runtable.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import argparse, sys
+from dstream.ds_api import death_star
+from pub_dbi        import pubdb_conn_info
+from pub_util       import pub_logger
+
+myparser = argparse.ArgumentParser(description='Create a run table.')
+
+myparser.add_argument('--name', dest='name', action='store',
+                      default='TestRun', type=str,
+                      help='Name of a run table to create/alter')
+
+myparser.add_argument('--nruns', dest='nruns', action='store',
+                      default=0, type=int,
+                      help='Number of runs to fill a new MainRun')
+
+myparser.add_argument('--nsubruns',dest='nsubruns',action='store',
+                      default=0, type=int,
+                      help='Number of sub-runs to fill a new MainRun')
+
+args = myparser.parse_args()
+
+logger = pub_logger.get_logger('death_star')
+k=death_star( pubdb_conn_info.admin_info(),
+              logger )
+
+if not k.connect():
+    sys.exit(1)
+
+k.create_death_star(args.name,args.nruns,args.nsubruns)
+
