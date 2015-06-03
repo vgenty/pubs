04b2c46a0e8d7070d834bba60fdd9a348e488f34
kterao <kterao@fnal.gov>
2015-06-02 21:29:42 -0500

    setup for evb & smc avoids those awkward setup lines specific to near1 --kazu

--- config/setup.sh
+++ config/setup.sh
@@ -58,30 +58,10 @@ case `uname -n` in
     (*ubdaq-prod*)
 	echo Setting up for ubdaq-prod machines...
         source /uboone/setup_online.sh
-	export UBOONEDAQ_HOME_DIR=${HOME}/development
-	cd $UBOONEDAQ_HOME_DIR/build
-	# Install will go to version specified in ../uboonedaq/projects/ups/product_deps
-	export CET_PRIVATE_UPS_DIR=${HOME}/install/privateups
-	export PRIVATE_UPS_DIR=${HOME}/development/install
-	source ../uboonedaq/projects/ups/setup_for_development -d;
-
-	source /uboone/larsoft/setup
 	source $PUB_TOP_DIR/config/ubdaq_conf.sh
-	export PRODUCTS=/home/echurch/larclient/prof.slf6.v04_05_00/localProducts_larsoft_v04_05_00_e7_prof:${PRODUCTS}
-# 	setup uboonecode v03_04_00 -q e6:prof
- 	setup uboonecode v04_05_00 -q e7:prof
-#	cd /home/echurch/larclient/prof.slf6.v04_05_00
-#	source localProducts_larsoft_v04_05_00_e7_prof/setup
-#	mrbslp
-        source /home/$USER/development/uboonedaq/projects/cpp2py/config/setup_cpp2py.sh
 	setup git
-	setup sam_web_client
-	setup ifdhc
 	setup psycopg2 v2_5_4
-	# setup postgres v9_2_4
         setup postgresql v9_3_6 -q p278
-	unsetup uboonedaq_datatypes
-	setup uboonedaq_datatypes v5_00_01 -qe7:prof
         export PYTHONPATH=${POSTGRESQL_LIBRARIES}/python2.7/site-packages:${PYTHONPATH}
 	export PUB_LOGGER_FILE_LOCATION=$PUB_TOP_DIR/log/`uname -n`
 	mkdir -p $PUB_LOGGER_FILE_LOCATION;
@@ -96,8 +76,30 @@ case `uname -n` in
 		export PUB_DAEMON_HANDLER_MODULE=dstream_online.evb_handler
 		;;
 	    (ubdaq-prod-near1)
+	        #
+                # This is not guaranteed to work (Kazu June-02-2015)
+                #
+
 		export PUB_DAEMON_LOG_MODULE=dstream_online.near1_logger
-		export PUB_DAEMON_HANDLER_MODULE=dstream_online.near1_handler		
+		export PUB_DAEMON_HANDLER_MODULE=dstream_online.near1_handler
+
+		export UBOONEDAQ_HOME_DIR=${HOME}/development
+		cd $UBOONEDAQ_HOME_DIR/build
+	        # Install will go to version specified in ../uboonedaq/projects/ups/product_deps
+		export CET_PRIVATE_UPS_DIR=${HOME}/install/privateups
+		export PRIVATE_UPS_DIR=${HOME}/development/install
+		source ../uboonedaq/projects/ups/setup_for_development -d;
+		
+		source /uboone/larsoft/setup
+		source $PUB_TOP_DIR/config/ubdaq_conf.sh
+		export PRODUCTS=/home/echurch/larclient/prof.slf6.v04_05_00/localProducts_larsoft_v04_05_00_e7_prof:${PRODUCTS}
+ 		setup uboonecode v04_05_00 -q e7:prof
+		source /home/$USER/development/uboonedaq/projects/cpp2py/config/setup_cpp2py.sh
+		setup sam_web_client
+		setup ifdhc
+		unsetup uboonedaq_datatypes
+		setup uboonedaq_datatypes v5_00_01 -qe7:prof
+
 		;;
 	esac
 	;;
