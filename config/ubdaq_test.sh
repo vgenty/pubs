d533ec2f12953d43bcdf488e8a6d57db079a8b0d
David Caratelli <davidc1@ubdaq-prod-smc.fnal.gov>
2015-06-06 11:11:51 -0500

    configuration script to be used for PUBS testing: connects to testprocdb database -- David Caratelli

---
+++ config/ubdaq_test.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+# SQL reader account config
+export PUB_PSQL_READER_HOST=192.168.144.189
+export PUB_PSQL_READER_PORT=""
+export PUB_PSQL_READER_USER=$USER
+export PUB_PSQL_READER_ROLE="uboonedaq_reader"
+export PUB_PSQL_READER_DB=testprocdb
+export PUB_PSQL_READER_PASS="${USER}argon!"
+export PUB_PSQL_READER_CONN_NTRY=10
+export PUB_PSQL_READER_CONN_SLEEP=10
+
+# SQL writer account config
+export PUB_PSQL_WRITER_HOST=192.168.144.189
+export PUB_PSQL_WRITER_PORT=""
+export PUB_PSQL_WRITER_USER=$USER
+export PUB_PSQL_WRITER_ROLE="uboonedaq_admin"
+export PUB_PSQL_WRITER_DB=testprocdb
+export PUB_PSQL_WRITER_PASS="${USER}argon!"
+export PUB_PSQL_WRITER_CONN_NTRY=10
+export PUB_PSQL_WRITER_CONN_SLEEP=10
+
+# SQL writer account config
+export PUB_PSQL_ADMIN_HOST=192.168.144.189
+export PUB_PSQL_ADMIN_PORT=""
+export PUB_PSQL_ADMIN_USER=$USER
+export PUB_PSQL_ADMIN_ROLE="uboonedaq_admin"
+export PUB_PSQL_ADMIN_DB=testprocdb
+export PUB_PSQL_ADMIN_PASS="${USER}argon!"
+export PUB_PSQL_ADMIN_CONN_NTRY=10
+export PUB_PSQL_ADMIN_CONN_SLEEP=10
+
+# SMTP account for sending an email report
+export PUB_SMTP_ACCT=uboonepro
+export PUB_SMTP_SRVR=smtp.gmail.com:587
+export PUB_SMTP_PASS=herbgreenlee
+
+#export PUB_SMTP_ACCT=drinkingkazu.pubs@aho.com
+#export PUB_SMTP_SRVR=smtp.gmail.com:tako
+#export PUB_SMTP_PASS=pubs.drinkingkazuak
