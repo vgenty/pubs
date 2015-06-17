cc3830ee0946ed52c48593733d8722674804c9b7
Herbert Greenlee <greenlee@fnal.gov>
2015-06-17 08:50:02 -0500

    Save job submit time in database. Update setup script.

--- config/setup.sh
+++ config/setup.sh
@@ -53,10 +53,8 @@ case `uname -n` in
 	    return;
 	fi
 	source $HOME/.sqlaccess/prod_access.sh
-	source /grid/fermiapp/products/uboone/setup_uboone.sh
-	setup psycopg2 v2_5_4
-	setup uboonecode v04_08_01 -q e7:prof
-	setup larbatch v01_09_00 
+	setup postgresql v9_3_6 -q p279
+	setup larbatch v01_10_00
 	export PUB_LOGGER_FILE_LOCATION=$PUB_TOP_DIR/log/`uname -n`
 	mkdir -p $PUB_LOGGER_FILE_LOCATION;
 	export PUB_DAEMON_LOG_MODULE=dstream_prod.gpvm_logger
