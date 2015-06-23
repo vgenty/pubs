b45063f4b55d9b86f29491bfe1b1f3300f88303e
David Caratelli <davidc1@ubdaq-prod-near1.fnal.gov>
2015-06-23 06:01:54 -0500

    register_new_run updated -- David Caratelli

--- dstream/register_new_run.py
+++ dstream/register_new_run.py
@@ -45,7 +45,8 @@ class register_new_run(ds_project_base):
 
         self._project = arg
 
-        self._data_dir = ''
+        self._data_dir = [] # list of directories where to find binary data
+        self_run_bound = [] # list of RUN numbers defining the upper bound of the run number in a given directory
         self._experts = ''
         self._runtable = ''
         
@@ -56,9 +57,10 @@ class register_new_run(ds_project_base):
 
         resource = self._api.get_resource( self._project )
 
-        self._data_dir = '%s' % (resource['DATADIR'])
-        self._experts = resource['EXPERTS']
-        self._runtable = resource['RUNTABLE']
+        self._data_dir  = resource['DATADIR'].split(":")
+        self._run_bound = [ int(x) for x in resource['RUNBOUND'].split(":") ]
+        self._experts   = resource['EXPERTS']
+        self._runtable  = resource['RUNTABLE']
 
         
     ## @brief access DB and retrieves new runs
@@ -67,113 +69,130 @@ class register_new_run(ds_project_base):
         # Attempt to connect DB. If failure, abort
         if not self.connect():
 	    self.error('Cannot connect to DB! Aborting...')
-	    return
-
-        # get ALL closed data files in DATADIR
-        if (os.path.isdir(self._data_dir) == False):
-            self.error('DATA DIR %s does not exist'%self._data_dir)
             return
 
-        self.info('Looking for data files in: %s'%self._data_dir)
 
-        dircontents = os.listdir(self._data_dir)
+        # loop through all directories in which data is to be found
+        for path_num in xrange(len(self._data_dir)):
 
-        # create a dictionary to keep track of
-        # - file name ----- NAME
-        # - run number ---- RUN
-        # - subrun number - SUBRUN
-        # - time-create --- TIMEC
-        # - time-modify --- TIMEM
-        # : dictionary key: ------ (RUN,SUBRUN)
-        # : dictionary content: -- (NAME,TIMEC,TIMEM)
-        file_info = {}
+            data_path = self._data_dir[path_num]
 
-        for f in dircontents:
+            self.info('Start access in data directory %s'%data_path)
+
+            run_lim = None
+            # if we are not yet at the last directory (for which there should be no run limit)
+            if ( path_num < len(self._run_bound) ):
+                run_lim = self._run_bound[path_num]
+                self.info('Run limit for this directory: %i'%run_lim)
+
+            # get ALL closed data files in DATADIR
+            if (os.path.isdir(data_path) == False):
+                self.error('DATA DIR %s does not exist'%data_path)
+                return
+
+            self.info('Looking for data files in: %s'%data_path)
+
+            dircontents = os.listdir(data_path)
+
+            # create a dictionary to keep track of
+            # - file name ----- NAME
+            # - run number ---- RUN
+            # - subrun number - SUBRUN
+            # - time-create --- TIMEC
+            # - time-modify --- TIMEM
+            # : dictionary key: ------ (RUN,SUBRUN)
+            # : dictionary content: -- (NAME,TIMEC,TIMEM)
+            file_info = {}
             
-            filepath = self._data_dir+'/'+f
-
-            # check that this is a file
-            if (os.path.isfile(filepath) == False):
-                continue
+            for f in dircontents:
             
-            try:
+                filepath = data_path+'/'+f
 
-                time_create  = os.path.getctime(filepath)
-                time_modify = os.path.getmtime(filepath)
+                # check that this is a file
+                if (os.path.isfile(filepath) == False):
+                    continue
+            
+                try:
+                    
+                    time_create  = os.path.getctime(filepath)
+                    time_modify = os.path.getmtime(filepath)
                 
-                # file format:
-                # NoiseRun-YYYY_M_DD_HH_MM_SS-RUN-SUBRUN.ubdaq
-                run    = int(f.replace('.ubdaq','').split('-')[-2])
-                subrun = int(f.replace('.ubdaq','').split('-')[-1])
-                self.info('found for run (%i, %i)'%(run,subrun))
+                    # file format:
+                    # NoiseRun-YYYY_M_DD_HH_MM_SS-RUN-SUBRUN.ubdaq
+                    run    = int(f.replace('.ubdaq','').split('-')[-2])
+                    subrun = int(f.replace('.ubdaq','').split('-')[-1])
+                    self.info('found for run (%i, %i)'%(run,subrun))
+                    
+                    file_info[tuple((run,subrun))] = [f,time_create,time_modify]
 
-                file_info[tuple((run,subrun))] = [f,time_create,time_modify]
-
-            except:
+                except:
                 
-                # if file-name is .ubdaq then we have a problem
-                # were not able to read run-subrun info from file
-                if (f.find('.ubdaq')):
-                    self.info('Could not read RUN/SUBRUN info for file %s'%f)
+                    # if file-name is .ubdaq then we have a problem
+                    # were not able to read run-subrun info from file
+                    if (f.find('.ubdaq')):
+                        self.info('Could not read RUN/SUBRUN info for file %s'%f)
 
-        # sort the dictionary
-        # we want to ignore the largest run/subrun information
-        # this will prevent us from potentially logging info
-        # for a file that has not yet been closed
-        sorted_file_info = sorted(file_info)
-        # get tuple with largest run/subrun info found in files
-        max_file_info = sorted_file_info[-1]
-
-        # fetch from database the last run/subrun number recorded
-        logger = pub_logger.get_logger('register_new_run')
-        reader = ds_api.ds_reader(pubdb_conn_info.reader_info(), logger)
-        last_recorded_info = reader.get_last_run_subrun('AhoRun')
-
-        # if we made it this far the file info needs to be
-        # recorded to the database
-        # DANGER *** DANGER *** DANGER *** DANGER
-        # we will now invoke the death_start
-        # this API will access the RUN table end edit
-        # informaton, which is exactly what we need to do
-        # however, this is dangerous and you should not
-        # copy this code and re-use it somewhere
-        # if you do, the Granduca's wrath will be upon you
-        # lucikly for you, the Granduca was the first to
-        # abolish the death penalty on November 30th 1786
-        # http://en.wikipedia.org/wiki/Grand_Duchy_of_Tuscany#Reform
-        # However, the imperial army may be less mercyful.
-        # DANGER *** DANGER *** DANGER *** DANGER
-        logger = pub_logger.get_logger('death_star')
-        rundbWriter = ds_api.death_star(pubdb_conn_info.admin_info(),logger)
-
-        # loop through dictionary keys and write to DB info
-        # for runs/subruns not yet stored
-        for info in sorted_file_info:
+            # sort the dictionary
+            # we want to ignore the largest run/subrun information
+            # this will prevent us from potentially logging info
+            # for a file that has not yet been closed
+            sorted_file_info = sorted(file_info)
+            # get tuple with largest run/subrun info found in files
+            max_file_info = sorted_file_info[-1]
             
-            # this key needs to be larger than the last logged value
-            # but less than the last element in the dictionary
-            if (info >= max_file_info):
-                continue;
-            if (info <= last_recorded_info):
-                continue;
+            # fetch from database the last run/subrun number recorded
+            logger = pub_logger.get_logger('register_new_run')
+            reader = ds_api.ds_reader(pubdb_conn_info.reader_info(), logger)
+            last_recorded_info = reader.get_last_run_subrun('AhoRun')
+            #last_recorded_info = (0,0)
 
-            # info is key (run,subrun)
-            # dictionary value @ key is array
-            # [file name, time_crate, time_modify]
-            run           = info[0]
-            subrun        = info[1]
-            run_info      = file_info[info]
-            file_creation = time.gmtime(int(run_info[1]))
-            file_closing  = time.gmtime(int(run_info[2]))
-            file_creation = time.strftime('%Y-%m-%d %H:%M:%S',file_creation)
-            file_closing  = time.strftime('%Y-%m-%d %H:%M:%S',file_closing)
+            # if we made it this far the file info needs to be
+            # recorded to the database
+            # DANGER *** DANGER *** DANGER *** DANGER
+            # we will now invoke the death_start
+            # this API will access the RUN table end edit
+            # informaton, which is exactly what we need to do
+            # however, this is dangerous and you should not
+            # copy this code and re-use it somewhere
+            # if you do, the Granduca's wrath will be upon you
+            # lucikly for you, the Granduca was the first to
+            # abolish the death penalty on November 30th 1786
+            # http://en.wikipedia.org/wiki/Grand_Duchy_of_Tuscany#Reform
+            # However, the imperial army may be less mercyful.
+            # DANGER *** DANGER *** DANGER *** DANGER
+            logger = pub_logger.get_logger('death_star')
+            rundbWriter = ds_api.death_star(pubdb_conn_info.admin_info(),logger)
+            
+            # loop through dictionary keys and write to DB info
+            # for runs/subruns not yet stored
+            for info in sorted_file_info:
+                
+                # this key needs to be larger than the last logged value
+                # but less than the last element in the dictionary
+                if (info >= max_file_info):
+                    continue;
+                if (run_lim):
+                    if ( info[0] > run_lim):
+                        continue;
+                if (info <= last_recorded_info):
+                    continue;
 
-            self.info('filling death star...')
-            # insert into the death start
-            rundbWriter.insert_into_death_star(self._runtable,info[0],info[1],
-                                               file_creation, file_closing)
-            # Report starting
-            self.info('recording info for new run: run=%d, subrun=%d ...' % (int(run),int(subrun)))
+                # info is key (run,subrun)
+                # dictionary value @ key is array
+                # [file name, time_crate, time_modify]
+                run           = info[0]
+                subrun        = info[1]
+                run_info      = file_info[info]
+                file_creation = time.gmtime(int(run_info[1]))
+                file_closing  = time.gmtime(int(run_info[2]))
+                file_creation = time.strftime('%Y-%m-%d %H:%M:%S',file_creation)
+                file_closing  = time.strftime('%Y-%m-%d %H:%M:%S',file_closing)
+                
+                self.info('filling death star...')
+                # insert into the death start
+                rundbWriter.insert_into_death_star(self._runtable,info[0],info[1],file_creation, file_closing)
+                # Report starting
+                self.info('recording info for new run: run=%d, subrun=%d ...' % (int(run),int(subrun)))
 
 
 
