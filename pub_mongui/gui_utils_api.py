f505e3a4097c1b28ec1e8877bf199e6b5bc4cda3
David Kaleko <kaleko@fnal.gov>
2015-07-30 17:19:31 -0500

    More updates to monitoring gui. piecharts have lots of slices. separate window that appears when you click on a pie chart now shows graph with one line for each status (so you can see some statuses decreasing, others increasing). still need to make a new background template and include newer relevant projects like transfer_pnnl

--- pub_mongui/gui_utils_api.py
+++ pub_mongui/gui_utils_api.py
@@ -25,16 +25,20 @@ class GuiUtilsAPI():
     #Dictionary that contains projects as keys, and arrays of statuses as values. ex:
     #{'dummy_daq': [(1, 109),(2,23)], 'dummy_nubin_xfer': [(0,144), (1, 109)]}
     self.proj_dict = self.dbi.list_status()
-
-    self.colors = [ 'b', 'g', 'r', 'y', 'm', 'o' ]
+    self.enabled_projects = [ x._project for x in self.dbi.list_projects() ]
+    self.colors = GuiUtils().getColors()
 
   def update(self):
     self.proj_dict = self.dbi.list_status()
+    self.enabled_projects = [ x._project for x in self.dbi.list_projects() ]
 
-  def getAllProjects(self):
+  def getAllProjectNames(self):
     return self.proj_dict.keys()
 
-  def newcomputePieSlices(self,projname):
+  def getEnabledProjectNames(self):
+    return self.enabled_projects
+
+  def computePieSlices(self,projname):
 
     if projname not in self.proj_dict.keys():
       print "Uh oh projname is not in dictionary. Figure out what happened..."
@@ -53,27 +57,14 @@ class GuiUtilsAPI():
     for x in statuses:
       #No slice for status == 0
       if not x[0]: continue
-      slices.append( ( (float(x[1])/tot_n), self.colors[len(slices)] ) )
+      if x[0] not in self.colors.keys():
+        print "uh oh! Status %d for project %s is not in my color dictionary. Adding it as red." % (x[0],projname)
+
+      mycolor = self.colors[x[0]] if x[0] in self.colors.keys() else 'r'
+      slices.append( ( (float(x[1])/tot_n), mycolor ) )
     
     return slices
       
-  def computePieSlices(self,projname,tot_n=0):
-
-    if tot_n:
-      #Status codes: 0 is completed (don't care about these) 1 is initiated, 2 is to be validated, >2 is error?
-      n_status1 = len(self.dbi.get_runs(projname,status=1))
-      n_status2 = len(self.dbi.get_runs(projname,status=2))
-      #Compute the fraction of the total runs that are complete
-      frac_1 = float(n_status1)/float(tot_n)
-      frac_2 = float(n_status2)/float(tot_n)
-      slices = [ (frac_1, 'b'), (frac_2, 'g') ]
-    else:
-      #If no runs have status 1 or 2, set pie chart to all red
-      # (though it will probably be drawn with 0 radius)
-      slices = [ (1., 'r') ]
-    
-    return slices
-
   def computePieRadius(self, projname, max_radius, tot_n=0):
 
     #If piechart has no entries, give it a zero radius
@@ -89,10 +80,28 @@ class GuiUtilsAPI():
 
   #Get the number of run/subruns that are relevant for pie charts
   # (don't care about fully completed run/subruns)
-  def getNRunSubruns(self,projname):
+  def getTotNRunSubruns(self,projname):
 
     statuses = self.proj_dict[projname]
     #statuses looks like [(0,15),(1,23),(2,333), (status, number_of_that_status)]
     #tot_n does not include status == 0
     tot_n = sum([x[1] for x in statuses if x[0]])
     return tot_n
+
+  def getNRunSubruns(self,projname):
+    #don't include status == 0 in any of this
+    return [x for x in self.proj_dict[projname] if x[0]]
+    
+
+class GuiUtils():
+  #Class that does NOT connect to any DB but just holds various constants/utility functions
+  def __init__(self):
+    #r, g, b, c, m, y, k, w    
+    self.colors = { 1:'b', 2:'g', 3: 'w', 4:'k', 100:'y', 102:'m', 65:'r', 101:'c', 404:'k', 999:'w', -9:'y' }
+    self.update_period = 10 #seconds
+
+  def getColors(self):
+    return self.colors
+
+  def getUpdatePeriod(self):
+    return self.update_period
