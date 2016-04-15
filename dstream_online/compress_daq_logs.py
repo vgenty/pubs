2739f60b3dca6aa2f9b16768172580d55e5cdf60
drinkingkazu <kazuhiro@nevis.columbia.edu>
2016-04-15 17:16:24 -0500

    script for compressing DAQ logs!

--- dstream_online/compress_daq_logs.py
+++ dstream_online/compress_daq_logs.py
@@ -11,12 +11,6 @@ class compress_daq_logs(ds_project_base):
 
     # Define project name as class attribute
     _project = 'compress_daq_logs'
-    _nruns   = 0
-    _in_dir  = ''
-    _out_dir = ''
-    _outfile_format = ''
-    _infile_foramt  = ''
-    _infile_age     = 0
 
     ## @brief default ctor can take # runs to process for this instance
     def __init__( self, arg = '' ):
@@ -25,12 +19,21 @@ class compress_daq_logs(ds_project_base):
         super( compress_daq_logs, self ).__init__( arg )
 
         self._project = str( arg )
+        self._nruns   = 0
+        self._in_dir  = ''
+        self._out_dir = ''
+        self._outfile_format = ''
+        self._infile_foramt  = ''
+        self._infile_age     = 0
+        self._data    = ''
+
         if not self.load_params():
             raise Exception()
 
     ## @brief load project parameters
     def load_params(self):
 
+        self.debug('Loading parameters...')
         # Attempt to connect DB. If failure, abort
         if not self.connect():
             self.error('Cannot connect to DB! Aborting...')
@@ -49,34 +52,43 @@ class compress_daq_logs(ds_project_base):
     ## @brief access DB and retrieves new runs
     def process_newruns(self):
         ctr = self._nruns
-        for x in self.get_runs(self._project, kSTATUS_INIT):
+        for x in self.get_runs(self._project, kSTATUS_INIT, False):
             # Break from loop if counter became 0
             if ctr <= 0: break
 
             ( run, subrun ) = ( int(x[0]), int(x[1]) )
+            # self.debug('Processing run %d, subrun %d...' %( run, subrun ) )
             if subrun > 0: continue
+            self.debug('Processing run %d, subrun %d...' %( run, subrun ) )
 
             # Counter decreases by 1
             ctr -= 1
 
-            flist = glob.glob( self._infile_format % run )
+            fFormat = self._infile_format % run
+            fFormat = '%s/%s' %( self._in_dir, fFormat )
+            # self.debug('Fileformat: %s ' % fFormat )
+            flist = glob.glob( fFormat )
+            # self.debug('Filelist for run %d, subrun %d: %r' %( run, subrun, flist ) )
             if flist:
-                date, hour, minute, second = find_run_time( flist[0] )
+                date, hour, minute, second = self.find_run_time( flist[0] )
+                self.debug('Run time: %s, %s:%s%s' %( date, hour, minute, second ) )
 
                 date_format = '%m.%d.%Y'
                 deltaT = datetime.datetime.today() - datetime.datetime.strptime( date, date_format )
+                self.debug('DeltaT: %s' % deltaT )
 
-                if deltaT.days > self._infile_age:
-                   outFile = process_files( self, run, date, hour, minute, second )
+                if deltaT.days > int(self._infile_age):
+                   outFile = self.process_files( run, date, hour, minute, second, flist )
 
         return
 
-    def process_files( self, run, date, hour, minute, second ):
+    def process_files( self, run, date, hour, minute, second, flist ):
         outName = self._outfile_format % ( run, date, hour, minute, second )
         outFile = '%s/%s' % ( self._out_dir, outName )
         if os.path.exists( outFile ):
             os.remove( outFile )
 
+        self.info('Compressing DAQ logs from run %d to %s...' %( run, outFile ) )
         tf = tarfile.open( outFile, 'w:bz2' )
         for f in flist:
             if os.path.exists( f ):
@@ -108,7 +120,7 @@ class compress_daq_logs(ds_project_base):
     ## @brief access DB and validate finished runs
     def validate( self ):
         ctr = self._nruns
-        for x in self.get_runs( self._project, kSTATUS_TO_BE_VALIDATED ):
+        for x in self.get_runs( self._project, kSTATUS_TO_BE_VALIDATED, False ):
             # Break from loop if counter became 0
             if ctr <= 0: break
 
@@ -118,9 +130,12 @@ class compress_daq_logs(ds_project_base):
             # Counter decreases by 1
             ctr -= 1
 
-            flist = glob.glob( self._infile_format % run )
+            fFormat = self._infile_format % run
+            fFormat = '%s/%s' %( self._in_dir, fFormat )
+            # self.debug('Fileformat: %s ' % fFormat )
+            flist = glob.glob( fFormat )
             if flist:
-                date, hour, minute, second = find_run_time( flist[0] )
+                date, hour, minute, second = self.find_run_time( flist[0] )
 
             outName = self._outfile_format % ( run, date, hour, minute, second )
             outFile = '%s/%s' % ( self._out_dir, outName )
@@ -129,13 +144,16 @@ class compress_daq_logs(ds_project_base):
                 if os.path.getsize( outFile ) == 0:
                     for f in flist:
                         if os.path.getsize( f ) > 0:
+                            self.info('DAQ logs from run %d have zero-size output %s, try again...' % ( run, outFile ) )
                             statusCode = kSTATUS_INIT
                             break
+                        self.info('Checked DAQ logs from run %d have nothing.' % run )
                         statusCode = kSTATUS_DONE
                 else:
                     # Remove the original DAQ logs
+                    self.info('Done with compressing DAQ logs from run %d!  Removing...' % run )
                     for f in flist:
-                        os.path.remove( f )
+                        os.remove( f )
                     check_flist = glob.glob( self._infile_format % run )
                     if len( check_flist ) == 0:
                         statusCode = kSTATUS_DONE
