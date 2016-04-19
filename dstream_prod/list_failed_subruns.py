e4f963451c87900888315273ef804ef6d7ab95a5
yuntse <yuntse@slac.stanford.edu>
2016-04-19 16:23:40 -0500

    Update failed subrun script.

--- dstream_prod/list_failed_subruns.py
+++ dstream_prod/list_failed_subruns.py
@@ -17,12 +17,27 @@ except:
     sys.exit(1)
 
 
-bad_runs = [5220, 5244, 5246, 5248, 5250, 5255, 5257, 5259, 5260]
+bad_runs = [5113, 5220, 5244, 5246, 5248, 5250, 5255, 5257, 5259, 5260,
+            5267, 5268, 5269,
+            5348, 5349, 5350, 5351, 5352,
+            5677, 5678,
+            5785, 5786, 5787, 5788, 5789, 5790, 5791, 5792, 5793, 5794,
+            5795, 5796, 5797, 5798, 5799, 5800, 5801, 5802, 5803, 5804,
+            5805, 5806, 5807, 5808, 5809, 5810, 5811, 5812, 5813, 5814,
+            5815, 5816, 5817, 5818]
+
+# Get project from command line (if any).
+
+prjname = None
+if len(sys.argv) > 1:
+    prjname = sys.argv[1]
 
 # Loop over projects.
 
 for probj in dbi.list_projects():
     project = probj._project
+    if prjname != None and prjname != project:
+        continue
     print '\nProject %s:' % project
     
     # Query failed subruns for this project.
