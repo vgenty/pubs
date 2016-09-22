a3d8be742bd7f9005e4d3e1ca90f474232774893
Michael Kirby <kirby@ubdaq-prod-ws01.fnal.gov>
2016-09-21 23:13:36 -0500

    updated metadata processing with run type infomation and better processing of the start and end time. put in function for timezone, but turned off now

--- dstream_online/get_metadata_no_hang.py
+++ dstream_online/get_metadata_no_hang.py
@@ -31,6 +31,18 @@ from scripts import find_run
 #  opens it and extracts some metadata,\n
 #  stuffs this into and writes out a json file.
 #  Next process registers the file with samweb *.ubdaq and mv's it to a dropbox directory for SAM to whisk it away...
+
+#There are several file types that we're going to start including in the metadata for run
+#PhysicsRun -> physics
+#TestRun -> test
+#NoiseRun -> noise
+#CalibrationRun -> calibration
+#PMTCalibrationRun -> pmtcalibration
+#TPCCalibrationRun -> tpccalibration
+#LaserCalibrationRun -> lasercalibration
+#BeamOff -> beamoff
+# unknown is if we get bad metadata from a file.
+
 class get_metadata( ds_project_base ):
 
 
@@ -331,6 +343,29 @@ class get_metadata( ds_project_base ):
 
             status_v.append((3,None))
             in_file = in_file_v[index_run]
+            in_file_split = in_file.split('-')
+            
+            if in_file_split[0]=='PhysicsRun':
+                run_type='physics'
+            elif in_file_split[0]=='NoiseRun':
+                run_type='noise'
+            elif in_file_split[0]=='CalibrationRun':
+                run_type='calibration'
+            elif in_file_split[0]=='PMTCalibrationRun':
+                run_type='pmtcalibration'
+            elif in_file_split[0]=='TPCCalibrationRun':
+                run_type='tpccalibration'
+            elif in_file_split[0]=='LaserCalibrationRun':
+                run_type='lasercalibration'
+            elif in_file_split[0]=='BeamOffRun':
+                run_type='beamoff'
+            elif in_file_split[0]=='BeamOff':
+                run_type='beamoff'
+            elif in_file_split[0]=='TestRun':
+                run_type='test'
+            else:
+                run_type='unknown'
+
             out,err = mp.communicate(index_run)
             fsize = os.path.getsize(in_file)
             run,subrun = runid_v[index_run]
@@ -357,8 +392,8 @@ class get_metadata( ds_project_base ):
             badJsonData = { 'file_name': os.path.basename(in_file), 
                             'file_type': "data", 
                             'file_size': fsize, 
-                            'file_format': "binaryraw-uncompressed", 
-                            'runs': [ [run,  subrun, 'test'] ], 
+                            'file_format': "binaryraw-compressed", 
+                            'runs': [ [run,  subrun, run_type] ], 
                             'first_event': 0,
                             'start_time': '1970-01-01T00:00:00',
                             'end_time': '1970-01-01T00:00:00',
@@ -404,7 +439,17 @@ class get_metadata( ds_project_base ):
             try:
                 read_gps=False
                 read_daqclock=False
-                for line in last_event_cout.split('\n'):
+                read_event_time=False
+                #This is the Offset since the system clock in the binary file is local time without any
+                #timezone information. But since SAM expects files  expect to either get UTC or times
+                #with timezone information, we've broken things a little bit.
+		utc_offset = 0
+                #if (time.daylight):
+                #    utc_offset=time.altzone
+		#else:
+		#    utc_offset=time.timezone
+
+		for line in last_event_cout.split('\n'):
                     if "run_number=" in line and "subrun" not in line :
                         self.debug('Extracting run_number... %s' % line.split('=')[-1])
                         if not run == int(line.split('=')[-1].replace(' ','')):
@@ -414,8 +459,22 @@ class get_metadata( ds_project_base ):
                         if not subrun == int(line.split('=')[-1].replace(' ','')):
                             self.warning('Detected un-matching subrun number: content says subrun=%s for file %s' % (int(line.split('=')[-1]),in_file))
                     if "event_number=" in line:
-                        self.debug('Extracting event_number... %s' % line.split('=')[-1])
+                        self.debug('Extracting Last event_number... %s' % line.split('=')[-1])
                         eevt = int(line.split('=')[-1])
+                    if "Event Time:" in line:
+                        read_event_time=True
+                    if "seconds" in line and "micro_seconds" in line and "nano_seconds" in line and read_event_time:
+                        words=line.split(' ')
+                        for w in words:
+                            if "seconds" in w and "micro_seconds" not in w and "nano_seconds" not in w:
+                                etime_secs_tmp = int(w.split('=')[1]) + utc_offset
+                            if "micro_seconds" in w:
+                                etime_usec = int(w.split('=')[1])
+
+                        self.debug('Extracted Last Event Time... %d%d' % (etime_secs_tmp, etime_usec) )
+                        etime = datetime.datetime.fromtimestamp(etime_secs_tmp).replace(microsecond=0).isoformat()
+                        etime_secs = (etime_secs_tmp, etime_usec)
+                        read_event_time=False
                     if "GPS Time FROM EVENT:Object" in line:
                         read_gps=True
                     if "GPS time (second,micro,nano)" in line and read_gps:
@@ -426,10 +485,63 @@ class get_metadata( ds_project_base ):
                             self.error('GPS time stamp format could not be interpreted...')
                             self.error(line)
                             raise DSException()
-                        self.debug('Extracted GPS Time... %s' % words)
-                        etime = datetime.datetime.fromtimestamp(words[0]).replace(microsecond=0).isoformat()
-                        etime_usec = words[1]
-                        etime_secs = (words[0], words[1])
+                        self.debug('Extracted Last GPS Time... %s' % words)
+                        gps_etime = datetime.datetime.fromtimestamp(words[0] + utc_offset).replace(microsecond=0).isoformat()
+                        gps_etime_usec = words[1]
+                        gps_etime_secs = (words[0] + utc_offset, words[1])
+                        read_gps=False
+                    if "Trigger Board Clock Time FROM EVENT:Object" in line:
+                        read_daqclock=True
+                    if "Trigger Clock: (frame,sample,div)" in line and read_daqclock:
+                        words=[]
+                        for w in line.split():
+                            if w.rstrip(',').isdigit(): words.append(int(w.rstrip(',')))
+                        if not len(words) == 3:
+                            self.error('DAQClock time last-event format could not be interpreted...')
+                            self.error(line)
+                            raise DSException()
+                        self.debug('Extracted Last DAQClock Time... %s' % words)
+                        edaqclock = (words[0] + utc_offset,words[1],words[2])
+                        read_daqclock=False
+                    if "daq_version_label=" in line:
+                        self.debug('DAQ version... %s' % line.split('=')[-1])
+                        ver = line.split('=')[-1]
+                        
+                read_gps=False
+                read_daqclock=False
+                read_event_time=False
+                for line in first_event_cout.split('\n'):
+                    if "event_number=" in line:
+                        self.debug('Extracting Start event_number... %s' % line.split('=')[-1])
+                        sevt = int(line.split('=')[-1].replace(' ',''))
+                    if "Event Time:" in line:
+                        read_event_time=True
+                    if "seconds" in line and "micro_seconds" in line and "nano_seconds" in line and read_event_time:
+                        words=line.split(' ')
+                        for w in words:
+                            if "seconds" in w and "micro_seconds" not in w and "nano_seconds" not in w:
+                                stime_secs_tmp = int(w.split('=')[1]) + utc_offset
+                            if "micro_seconds" in w:
+                                stime_usec = int(w.split('=')[1])
+
+                        self.debug('Extracted Start Event Time... %d %d' % (stime_secs_tmp, stime_usec) )
+                        stime = datetime.datetime.fromtimestamp(stime_secs_tmp).replace(microsecond=0).isoformat()
+                        stime_secs = (stime_secs_tmp, stime_usec)
+                        read_event_time=False
+                    if "GPS Time FROM EVENT:Object" in line:
+                        read_gps=True
+                    if "GPS time (second,micro,nano)" in line and read_gps:
+                        words=[]
+                        for w in line.split():
+                            if w.rstrip(',').isdigit(): words.append(int(w.rstrip(',')))
+                        if not len(words) == 3:
+                            self.error('GPS time stamp format could not be interpreted...')
+                            self.error(line)
+                            raise DSException()
+                        self.debug('Extracted Start GPS Time... %s' % words)
+                        gps_stime = datetime.datetime.fromtimestamp(words[0] + utc_offset).replace(microsecond=0).isoformat()
+                        gps_stime_usec = words[1]
+                        gps_stime_secs = (words[0] + utc_offset, words[1])
                         read_gps=False
                     if "Trigger Board Clock Time FROM EVENT:Object" in line:
                         read_daqclock=True
@@ -442,57 +554,19 @@ class get_metadata( ds_project_base ):
                             self.error(line)
                             raise DSException()
                         self.debug('Extracted DAQClock Time... %s' % words)
-                        edaqclock = (words[0],words[1],words[2])
-                        read_daqclock=False
-                    if "daq_version_label=" in line:
-                        self.debug('DAQ version... %s' % line.split('=')[-1])
-                        ver = line.split('=')[-1]
-                        
-                read_gps=False
-                read_daqclock=False
-                for line in first_event_cout.split('\n'):
-                    if "event_number=" in line:
-                        self.debug('Extracting event_number... %s' % line.split('=')[-1])
-                        sevt = int(line.split('=')[-1].replace(' ',''))
-                    if "GPS Time FROM EVENT:Object" in line:
-                        read_gps=True
-                    if "GPS time (second,micro,nano)" in line and read_gps:
-                        words=[]
-                        for w in line.split():
-                            if w.rstrip(',').isdigit(): words.append(int(w.rstrip(',')))
-                        if not len(words) == 3:
-                            self.error('GPS time stamp format could not be interpreted...')
-                            self.error(line)
-                            raise DSException()
-                        self.debug('Extracted GPS Time... %s' % words)
-                        stime = datetime.datetime.fromtimestamp(words[0]).replace(microsecond=0).isoformat()
-                        stime_usec = words[1]
-                        stime_secs = (words[0], words[1])
-                        read_gps=False
-                    if "Trigger Board Clock Time FROM EVENT:Object" in line:
-                        read_daqclock=True
-                    if "Trigger Clock: (frame,sample,div)" in line and read_daqclock:
-                        words=[]
-                        for w in line.split():
-                            if w.rstrip(',').isdigit(): words.append(int(w.rstrip(',')))
-                        if not len(words) == 3:
-                            self.error('DAQClock time last-event format could not be interpreted...')
-                            self.error(line)
-                            raise DSException()
-                        self.debug('Extracted DAQClock Time... %s' % words)
-                        sdaqclock = (words[0],words[1],words[2])
+                        sdaqclock = (words[0] + utc_offset,words[1],words[2])
                         read_daqclock=False
 
                 # Need to fix it cuz it's pre-PPS
                 # Gotta get out of isoformat strings to do the maths.
-                if datetime.datetime.fromtimestamp(stime_secs[0]).replace(microsecond=0).year < 2015 and edaqclock is not -1 and sdaqclock is not -1 and etime is not -1 and stime is not -1:  
-                    start_prePPS = stime
+                if datetime.datetime.fromtimestamp(gps_stime_secs[0]).replace(microsecond=0).year < 2015 and edaqclock is not -1 and sdaqclock is not -1 and gps_etime is not -1 and gps_stime is not -1:  
+                    start_prePPS = gps_stime
                     dt = sum([i*j for (i, j) in zip(tuple(map(lambda x, y: x - y, edaqclock, sdaqclock)) , (1600.0,0.5,0.00624)) ]) # dot-product, musec
-                    stime_tmp = etime_secs[0]*1.0E6 + etime_secs[1] - dt
+                    gps_stime_tmp = gps_etime_secs[0]*1.0E6 + gps_etime_secs[1] - dt
                     #pdb.set_trace()
-                    stime = datetime.datetime.fromtimestamp(int(stime_tmp * 1.0E-6)).replace(microsecond=0).isoformat()
-                    stime_usec = str ( int (stime_tmp - ( int(stime_tmp *1.0E-6) * 1.0E6)) )
-                    self.info('Changed start time from ' + start_prePPS + ' to ' + stime + ' and ' + stime_usec + ' microseconds.')
+                    gps_stime = datetime.datetime.fromtimestamp(int(gps_stime_tmp * 1.0E-6)).replace(microsecond=0).isoformat()
+                    gps_stime_usec = str ( int (gps_stime_tmp - ( int(gps_stime_tmp *1.0E-6) * 1.0E6)) )
+                    self.info('Changed start time from ' + start_prePPS + ' to ' + gps_stime + ' and ' + gps_stime_usec + ' microseconds.')
 
                 status_v[index_run] = (3,None)
                 self.info('Successfully extract metadata for run=%d subrun=%d: %s @ %s' % (run,subrun,in_file,time.strftime('%Y-%m-%d %H:%M:%S')))
@@ -504,16 +578,27 @@ class get_metadata( ds_project_base ):
                 status_v[i] = (kSTATUS_ERROR_CANNOT_MAKE_BIN_METADATA,badJsonData)
                 self.error('Failed extracting metadata: %s' % in_file)
                 continue
+
+            if ( (gps_stime_sec[0] - stime_secs[0]) > 100):
+                self.error("The GPS Event time was more than 100 seconds different than the localhost time from NTP!!!!")
+                self.error("We are gonna use the localhost NTP time for online microseconds")
+                gps_stime_usec = stime_usec
+                
+            if ( ( gps_etime_sec[0] - etime_secs[0]) > 100 ):
+                self.error("The GPS Event time was more than 100 seconds different than the localhost time from NTP!!!!")
+                self.error("We are gonna use the localhost NTP time for online microseconds")
+                gps_etime_usec = etime_usec
+
             num_events = eevt - sevt + 1
-            if num_events < 0: num_events = 0
+            if num_events < 0: num_events = 0            
             # run number and subrun number in the metadata seem to be funny,
             # and currently we are using the values in the file name.
             # Also add ub_project.name/stage/version, and data_tier by hand
             jsonData = { 'file_name': os.path.basename(in_file), 
                          'file_type': "data", 
                          'file_size': fsize, 
-                         'file_format': "binaryraw-uncompressed", 
-                         'runs': [ [run,  subrun, 'test'] ], 
+                         'file_format': "binaryraw-compressed", 
+                         'runs': [ [run,  subrun, run_type] ], 
                          'first_event': sevt, 
                          'start_time': stime, 
                          'end_time': etime, 
@@ -525,8 +610,8 @@ class get_metadata( ds_project_base ):
                          "ub_project.name": "online", 
                          "ub_project.stage": "assembler", 
                          "ub_project.version": self._pubsver,
-                         'online.start_time_usec': str(stime_usec),
-                         'online.end_time_usec': str(etime_usec)}
+                         'online.start_time_usec': str(gps_stime_usec),
+                         'online.end_time_usec': str(gps_etime_usec)}
             status_v[index_run] = (kSTATUS_TO_BE_VALIDATED,jsonData)
 
         return status_v
